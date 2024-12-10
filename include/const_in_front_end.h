660796e4c7a940c8655ec80dc980b8506d299010
Alexandr26Starostin <sanchostarostin@gmail.com>
2024-12-10 14:33:03 +0300

    begin front_end

---
+++ include/const_in_front_end.h
@@ -0,0 +1,6 @@
+#ifndef CONST_IN_FRONT_END_H
+#define CONST_IN_FRONT_END_H
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "const_in_front_end.h"
+
+int main ()
+{
+	printf ("Привет!\n");
+	
+	return 0;
+}
